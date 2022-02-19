70979593fa67e37c9d19f21b5e764cd3921af99a
Voicecode B.V <michael@voicecode.nl>
2022-02-19 09:36:11 +0100

    [Feature] Send timestamps with app data

--- src/Helpers/RequestHelper.php
+++ src/Helpers/RequestHelper.php
@@ -8,24 +8,30 @@ class RequestHelper
 {
     public function pushApplicationData($deployment = false)
     {
+        // Return when no site token is available.
         if (is_null(config('dashboard-connector.site_token'))) {
-            if ($deployment == false) {
-                $this->error('No site token available!');
-            }
-
             return;
         }
 
+        // Collect app data.
         $data = [
             'token' => config('dashboard-connector.site_token'),
             'laravel_version' => app()->version(),
             'php_version' => phpversion(),
+            'pinged_at' => now()->format('Y-m-d H:i:s'),
         ];
 
+        // Only add deployed at timestamp when this request
+        // has been triggered via a deployment.
         if ($deployment) {
-            $data['deployed_at'] = now();
+            $data['deployed_at'] = now()->format('Y-m-d H:i:s');
         }
 
-        Http::put('https://dashboard.codingmonkeys.nl/api/v1/update', $data);
+        $domain = app()->isLocal()
+            ? 'dashboard.test'
+            : 'dashboard.codingmonkeys.nl';
+
+        // Send data to dashboard.
+        Http::put('https://'.$domain.'/api/v1/update', $data);
     }
 }
